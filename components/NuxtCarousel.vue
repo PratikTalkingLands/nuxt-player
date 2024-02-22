af650bfbb5a80c10923156fdcfab555dbb221273
Pratik Chakraborty <pratik@talkinglands.com>
2024-02-23 00:29:46 +0530

    initial commit

--- components/NuxtCarousel.vue
+++ components/NuxtCarousel.vue
@@ -51,7 +51,7 @@
   </template>
   
   <script setup>
-  import { ref, computed } from 'vue';
+  import { ref, computed, defineProps } from 'vue';
   
   const props = defineProps(["items"])
   const items = props?.items
